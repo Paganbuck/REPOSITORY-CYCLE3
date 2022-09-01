29c1b22c004a789a6f0051d94e33ebd35684250d
nelsonDeveloper41 <nelsondeveloper369@gmail.com>
2022-09-01 11:17:47 -0500

    Inicializando Proyecto con Spring Boot

---
+++ ProyectoArray/src/main/java/com/Ciclo3/ProyectoArray/ProyectoArrayApplication.java
@@ -0,0 +1,13 @@
+package com.Ciclo3.ProyectoArray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoArrayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoArrayApplication.class, args);
+	}
+
+}
