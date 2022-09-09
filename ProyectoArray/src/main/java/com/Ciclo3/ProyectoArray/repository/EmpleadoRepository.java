f34acc62084eedeec57ca76a50456bb57ab3c3c7
k0106 <kellygaviria1@gmail.com>
2022-09-09 17:11:04 -0500

    Se inicia el codigo de EmpresaReposit Se inicia codigo de EmpresaRepository.java Se crea la Interfaz de EmpleadoRepository.java

---
+++ ProyectoArray/src/main/java/com/Ciclo3/ProyectoArray/repository/EmpleadoRepository.java
@@ -0,0 +1,9 @@
+package com.Ciclo3.ProyectoArray.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmpleadoRepository extends CrudRepository<Empleado,Integer>{
+
+}
+
+
