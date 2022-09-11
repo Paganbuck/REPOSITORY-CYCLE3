8241be30bdacb44232bce5f7707a29ea5937ff8f
econowil <econowil@hotmail.com>
2022-09-11 13:26:07 -0500

    Se agregan atributos a Empleado.java

--- ProyectoArray/src/main/java/com/Ciclo3/ProyectoArray/models/Empleado.java
+++ ProyectoArray/src/main/java/com/Ciclo3/ProyectoArray/models/Empleado.java
@@ -1,8 +1,17 @@
 package com.Ciclo3.ProyectoArray.models;
 
+import javax.persistence.*;
 
+@Entity
+@Table(name="Empleado")
 public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String nombre;
+    private String correo;
+    @ManyToOne
+    @JoinColumn(name = "empresa_id")
+    private Empresa empresa;
+    private String rol;
 
-
-
-}
