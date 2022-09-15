02339180d653f270285b09646fdf31f8466cb443
nelsonDeveloper41 <nelsondeveloper369@gmail.com>
2022-09-15 09:47:59 -0500

    Se actualiza el controlador para empresa

--- ProyectoArray/src/main/java/com/Ciclo3/ProyectoArray/controller/Controlador.java
+++ ProyectoArray/src/main/java/com/Ciclo3/ProyectoArray/controller/Controlador.java
@@ -1,10 +1,11 @@
 package com.Ciclo3.ProyectoArray.controller;
 
 ///a partri de este momento modificacmos el fullcontrller por el RESTcontroller Y RAMA controller_typeREST
-//agregamos otro cambio antes de iniciar con el RESTcontroller
+//cambiamos RESTcontroller por Controller Full
 import com.Ciclo3.ProyectoArray.models.Empleado;
 import com.Ciclo3.ProyectoArray.models.Empresa;
 import com.Ciclo3.ProyectoArray.models.MovimientoDinero;
+import com.Ciclo3.ProyectoArray.repository.MovimientosRepository;
 import com.Ciclo3.ProyectoArray.services.EmpleadoService;
 import com.Ciclo3.ProyectoArray.services.EmpresaService;
 import com.Ciclo3.ProyectoArray.services.MovimientosService;
@@ -18,19 +19,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 public class Controlador {
 
     //conectamos cada controlador a su respectivo servicio
     @Autowired
     EmpresaService empresaService;
 
+    //conectamos cada controlador a su respectivo servicio
     @Autowired
     EmpleadoService empleadoService;
 
+    //conectamos cada controlador a su respectivo servicio
     @Autowired
     MovimientosService movimientosService;
-    //conectamos cada controlador a su respectivo servicio
+
+    @Autowired
+    MovimientosRepository movimientosReposito;
 
 
 
@@ -38,41 +43,53 @@ public class Controlador {
      *************************************************************************************************
      * **********************************************************************************************
      */
-    @GetMapping("/enterprises") //Ver json de todas las empresas
-    public List<Empresa> verEmpresas() {
-        return empresaService.listarAllEmpresas();
+    @GetMapping({"/","/VerEmpresas"})
+    public String viewEmpresas(Model model, @ModelAttribute("mensaje") String mensaje) {
+        List<Empresa> listaEmpresas = empresaService.getAllEmpresas();
+        model.addAttribute("emplist", listaEmpresas);
+        model.addAttribute("mensaje", mensaje);
+        return "verEmpresas"; //Llamamos al HTML
     }
 
-    @PostMapping("/enterprises") //Guardar el json del body como una nueva empresa o registro en nuestra bd
-    public Empresa guardarEmpresa(@RequestBody Empresa emp){
-        return this.empresaService.crearOactualizarEmpresa(emp);
-    }
-
-    @GetMapping(path = "enterprises/{id}")
-    public Empresa empresaPorID(@PathVariable("id") Integer id){
-        return this.empresaService.consultarEmpresaXId(id);
-    }
-
-    @PatchMapping("/enterprises/{id}")
-    public Empresa actualizarEmpresa(@PathVariable("id") Integer id, @RequestBody Empresa empresa) {
-        Empresa emp = empresaService.consultarEmpresaXId(id);
-        emp.setNombre(empresa.getNombre());
-        emp.setDireccion(empresa.getDireccion());
-        emp.setTelefono(empresa.getTelefono());
-        emp.setNIT(empresa.getNIT());
-        return empresaService.crearOactualizarEmpresa(emp);
-    }
-
-
-    @DeleteMapping (path= "enterprises/{id}") //Eliminar registro de la bd
-    public String DeleteEmpresa(@PathVariable("id") Integer id){
-        boolean respuesta= this.empresaService.deleteEmpresa(id);
-        if (respuesta){  //Si respuesta es true?
-            return "Se elimino la empresa con id" +id;
+    @PostMapping("/GuardarEmpresa")
+    public String guardarEmpresa(Empresa emp, RedirectAttributes redirectAttributes){
+        if(empresaService.saveOrUpdateEmpresa(emp)==true){
+            redirectAttributes.addFlashAttribute("mensaje","saveOK");
+            return "redirect:/VerEmpresas";
         }
-        else{
-            return "No se pudo eliminar la empresa con id"+id;
+        redirectAttributes.addFlashAttribute("mensaje","saveError");
+        return "redirect:/AgregarEmpresa";
+    }
+
+    @GetMapping("/EditarEmpresa/{id}")
+    public String editarEmpresa(Model model, @PathVariable Integer id, @ModelAttribute("mensaje") String mensaje){
+        Empresa emp=empresaService.getEmpresaById(id);
+        //Creamos un atributo para el modelo, que se llame igualmente emp y es el que ira al html para llenar o alimentar campos
+        model.addAttribute("emp",emp);
+        model.addAttribute("mensaje", mensaje);
+        return "editarEmpresa";
+    }
+
+
+    @PostMapping("/ActualizarEmpresa")
+    public String updateEmpresa(@ModelAttribute("emp") Empresa emp, RedirectAttributes redirectAttributes){
+        if(empresaService.saveOrUpdateEmpresa(emp)){
+            redirectAttributes.addFlashAttribute("mensaje","updateOK");
+            return "redirect:/VerEmpresas";
         }
+        redirectAttributes.addFlashAttribute("mensaje","updateError");
+        return "redirect:/EditarEmpresa/"+emp.getId();
+
+    }
+
+    @GetMapping("/EliminarEmpresa/{id}")
+    public String eliminarEmpresa(@PathVariable Integer id, RedirectAttributes redirectAttributes){
+        if (empresaService.deleteEmpresa(id)==true){
+            redirectAttributes.addFlashAttribute("mensaje","deleteOK");
+            return "redirect:/VerEmpresas";
+        }
+        redirectAttributes.addFlashAttribute("mensaje", "deleteError");
+        return "redirect:/VerEmpresas";
     }
     /*FINAL EMPRESAS***************************************************************************************
      *************************************************************************************************
@@ -85,44 +102,7 @@ public class Controlador {
      *************************************************************************************************
      * **********************************************************************************************
      */
-    @GetMapping("/empleados") //Ver json de todas los empleados
-    public List<Empleado> verEmpleados(){
-        return empleadoService.getAllEmpleado();
-    }
 
-    @PostMapping("/empleados") //Guardar un empleado nuevo
-    public Optional<Empleado> guardarEmpleado(@RequestBody Empleado empl){
-        return Optional.ofNullable(this.empleadoService.saveOrUpdateEmpleado(empl));
-    }
-
-    @GetMapping(path = "empleados/{id}")//Consultar empleado por ID
-    public Optional<Empleado> empleadoPorID(@PathVariable("id") Integer id){
-        return this.empleadoService.getEmpleadoById(id);
-    }
-
-    @GetMapping("/enterprises/{id}/empleados")// Consultar empleados por empresa
-    public ArrayList<Empleado> EmpleadoPorEmpresa(@PathVariable("id") Integer id){
-        return this.empleadoService.obtenerPorEmpresa(id);
-    }
-
-    @PatchMapping("/empleados/{id}")
-    public Empleado actualizarEmpleado(@PathVariable("id") Integer id, @RequestBody Empleado empleado){
-        Empleado empl=empleadoService.getEmpleadoById(id).get();
-        empl.setNombre(empleado.getNombre());
-        empl.setCorreo(empleado.getCorreo());
-        empl.setEmpresa(empleado.getEmpresa());
-        empl.setRol(empleado.getRol());
-        return empleadoService.saveOrUpdateEmpleado(empl);
-    }
-
-    @DeleteMapping("/empleados/{id}") //Metodo para eliminar empleados por id
-    public String DeleteEmpleado(@PathVariable("id") Integer id){
-        boolean respuesta=empleadoService.deleteEmpleado(id); //eliminamos usando el servicio de nuestro service
-        if (respuesta){ //si la respuesta booleana es true, si se eliminò
-            return "Se pudo eliminar correctamente el empleado con id "+id;
-        }//Si la respuesta booleana es false, no se eliminó
-        return "No se puedo eliminar correctamente el empleado con id "+id;
-    }
     /*FINAL EMPLEADO***************************************************************************************
      *************************************************************************************************
      * **********************************************************************************************
@@ -133,48 +113,7 @@ public class Controlador {
      *************************************************************************************************
      * **********************************************************************************************
      */
-    @GetMapping("/movimientos") //Consultar todos los movimientos
-    public List<MovimientoDinero> verMovimientos(){
-        return movimientosService.getAllMovimientos();
-    }
 
-    @PostMapping("/movimientos")///crear o guardar un nuevo movimiento
-    public MovimientoDinero guardarMovimiento(@RequestBody MovimientoDinero movimiento){
-        return movimientosService.saveOrUpdateMovimiento(movimiento);
-    }
-
-    @GetMapping("/movimientos/{id}") //Consultar movimiento por id
-    public MovimientoDinero movimientoPorId(@PathVariable("id") Integer id){
-        return movimientosService.getMovimientoById(id);
-    }
-
-    @PatchMapping("/movimientos/{id}")//Editar o actualizar un movimiento
-    public MovimientoDinero actualizarMovimiento(@PathVariable("id") Integer id, @RequestBody MovimientoDinero movimiento){
-        MovimientoDinero mov=movimientosService.getMovimientoById(id);
-        mov.setConcepto(movimiento.getConcepto());
-        mov.setMonto(movimiento.getMonto());
-        mov.setUsuario(movimiento.getUsuario());
-        return movimientosService.saveOrUpdateMovimiento(mov);
-    }
-
-    @DeleteMapping("/movimientos/{id}")
-    public String deleteMovimiento(@PathVariable("id") Integer id){
-        boolean respuesta= movimientosService.deleteMovimiento(id);
-        if (respuesta){
-            return "Se elimino correctamente el movimiento con id " +id;
-        }
-        return "No se pudo eliminar el movimiento con id "+id;
-    }
-
-    @GetMapping("/empleados/{id}/movimientos") //Consultar movimientos por id del empleado
-    public ArrayList<MovimientoDinero> movimientosPorEmpleado(@PathVariable("id") Integer id){
-        return movimientosService.obtenerPorEmpleado(id);
-    }
-
-    @GetMapping("/enterprises/{id}/movimientos") //Consultar movimientos que pertenecen a una empresa por el id de la empresa
-    public ArrayList<MovimientoDinero> movimientosPorEmpresa(@PathVariable("id") Integer id){
-        return movimientosService.obtenerPorEmpresa(id);
-    }
     /*FINAL MOVIMIENTO DINERO***************************************************************************************
      *************************************************************************************************
      * **********************************************************************************************
