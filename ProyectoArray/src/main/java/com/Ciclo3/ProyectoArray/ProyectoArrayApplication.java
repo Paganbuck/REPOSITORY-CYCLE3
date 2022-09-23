a4d74a76909dc8b6ce62ccf75d7512aa5b7d15e4
k0106 <kellygaviria1@gmail.com>
2022-09-22 21:34:47 -0500

    modificando codigo.

--- ProyectoArray/src/main/java/com/Ciclo3/ProyectoArray/ProyectoArrayApplication.java
+++ ProyectoArray/src/main/java/com/Ciclo3/ProyectoArray/ProyectoArrayApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @SpringBootApplication
-public class Ciclo3Application {
+public class ProyectoArrayApplication {
 
 	@GetMapping("/hello")
 	public String hello(){
@@ -22,7 +22,7 @@ public class Ciclo3Application {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ciclo3Application.class, args);
+		SpringApplication.run(ProyectoArrayApplication.class, args);
 	}
 
 }
